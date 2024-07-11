07f05c96330639fa0fb7b76a9132fa858d8b291d
louscmh <louiscmh42@gmail.com>
2024-07-11 15:08:07 +0800

    - Implented `WINNER` - Updated `seeds.json` - Added obs scene ready for matches

--- WINNER/index.js
+++ WINNER/index.js
@@ -11,23 +11,19 @@ socket.onerror = error => {
     console.log("Socket Error: ", error);
 };
 
-// BEATMAP DATA /////////////////////////////////////////////////////////////////
-let beatmapSet = [];
-let beatmaps = [];
+// SEED DATA /////////////////////////////////////////////////////////////////
+let seeds = [];
 (async () => {
     try {
-        const jsonData = await $.getJSON("../_data/beatmaps.json");
-        jsonData.map((beatmap) => {
-            beatmapSet.push(beatmap);
+        const jsonData = await $.getJSON("../_data/seeds.json");
+        jsonData.map((seed) => {
+            seeds.push(seed);
         });
     } catch (error) {
         console.error("Could not read JSON file", error);
     }
-    for (index = 0; index < beatmapSet.length; index++) {
-        beatmaps.push(beatmapSet[index]["beatmapId"]);
-    }
 })();
-console.log(beatmapSet);
+console.log(seeds);
 
 // SHOWCASE DATES DATA /////////////////////////////////////////////////////////////////
 let dates = [];
@@ -44,73 +40,49 @@ let dates = [];
 console.log(dates);
 
 // HTML VARS /////////////////////////////////////////////////////////////////
-let beatmapTitle = document.getElementById("songName");
-let beatmapArtist = document.getElementById("artistName");
-let beatmapMapper = document.getElementById("mapperName");
-let pickID = document.getElementById("pickID");
-let mapOD = document.getElementById("mapOD");
-let mapSR = document.getElementById("mapSR");
-let mapBPM = document.getElementById("mapBPM");
-let mapLength = document.getElementById("mapLength");
+let teamName = document.getElementById("teamName");
+let playerOne = document.getElementById("playerOne");
+let playerTwo = document.getElementById("playerTwo");
 let stageText = document.getElementById("stageText");
-let statDetails = document.getElementById("statDetails");
-
-let beatmapTitleDelay = document.getElementById("songDelay");
-let beatmapArtistDelay = document.getElementById("artistDelay");
 
 // PLACEHOLDER VARS /////////////////////////////////////////////////////////////////
 let currentFile = "";
 let gameState;
 let currentStage;
+let bestOfTemp;
+let scoreBlueTemp;
+let scoreRedTemp;
 
 socket.onmessage = event => {
     let data = JSON.parse(event.data);
-
-    let file = data.menu.bm.path.file;
-    if (currentFile != file) {
-        updateDetails(data);
-    }
 
     if (currentStage != getCurrentStage()) {
         currentStage = getCurrentStage()
         stageText.innerHTML = currentStage;
     }
-}
 
-async function updateDetails(data) {
-	let { id } = data.menu.bm;
-	let { memoryOD, fullSR, BPM: { min, max } } = data.menu.bm.stats;
-	let { full } = data.menu.bm.time;
-    let { mapper, artist, title } = data.menu.bm.metadata;
-    let file = data.menu.bm.path.file;
-    let pick;
+    if (bestOfTemp !== Math.ceil(data.tourney.manager.bestOF / 2) || scoreBlueTemp !== data.tourney.manager.stars.left || scoreRedTemp !== data.tourney.manager.stars.right) {
 
-    console.log(file);
+		// Courtesy of Victim-Crasher
+		bestOfTemp = Math.ceil(data.tourney.manager.bestOF / 2);
+        scoreBlueTemp = data.tourney.manager.stars.left;
+        scoreRedTemp = data.tourney.manager.stars.right;
 
-    // CHECKER FOR MAPPICK
-    if (beatmaps.includes(id)) {
-        pick = beatmapSet.find(beatmap => beatmap["beatmapId"] === id)["pick"];
-    } else if (beatmaps.includes(file)) {
-        pick = beatmapSet.find(beatmap => beatmap["beatmapId"] === file)["pick"];
-    }
-    pickID.innerHTML = pick == null ? "XX1" : pick;
+        if (bestOfTemp == scoreBlueTemp) {
+            teamName.innerHTML = data.tourney.manager.teamName.left;
+            playerOne.innerHTML = seeds.find(seed => seed["Team"] === teamName.innerHTML)["playerOne"];
+            playerTwo.innerHTML = seeds.find(seed => seed["Team"] === teamName.innerHTML)["playerTwo"];
+        } else if (bestOfTemp == scoreRedTemp) {
+            teamName.innerHTML = data.tourney.manager.teamName.right;
+            playerOne.innerHTML = seeds.find(seed => seed["Team"] === teamName.innerHTML)["playerOne"];
+            playerTwo.innerHTML = seeds.find(seed => seed["Team"] === teamName.innerHTML)["playerTwo"];
+        } else {
+            teamName.innerHTML = "";
+            playerOne.innerHTML = "Match has not ended yet!"
+            playerTwo.innerHTML = ""
+        }
+	}
 
-    beatmapTitle.innerHTML = title;
-    beatmapArtist.innerHTML = artist;
-    beatmapMapper.innerHTML = mapper;
-    mapOD.innerHTML = memoryOD.toFixed(1);
-    mapSR.innerHTML = fullSR.toFixed(2);
-    mapBPM.innerHTML = min === max ? min : `${min} - ${max}`;
-    mapLength.innerHTML = parseTime(full);
-
-    makeScrollingText(beatmapTitle, beatmapTitleDelay,20,620,40);
-    makeScrollingText(beatmapArtist, beatmapArtistDelay,20,620,40);
-}
-
-const parseTime = ms => {
-	const second = Math.floor(ms / 1000) % 60 + '';
-	const minute = Math.floor(ms / 1000 / 60) + '';
-	return `${'0'.repeat(2 - minute.length) + minute}:${'0'.repeat(2 - second.length) + second}`;
 }
 
 function getCurrentStage() {
@@ -118,14 +90,17 @@ function getCurrentStage() {
     var day = date.getUTCDate();
     var month = date.getUTCMonth()+1;
 
-    console.log(`${day}, ${month}`);
+    // console.log(`${day}, ${month}`);
+
+    let currentStage;
 
     for (let stage of dates) {
         stageDate = parseDateTime(stage["date"]);
         // console.log(`${stageDate.getUTCDate()}, ${stageDate.getUTCMonth()}`);
-        if (stageDate.getUTCDate() == day && stageDate.getUTCMonth()+1 == month) {
-            return stage["stage"];
+        if (stageDate.getUTCDate() >= day && stageDate.getUTCMonth()+1 >= month) {
+            return currentStage["stage"];
         }
+        currentStage = stage;
     }
     return "No Stage Detected";
 }
@@ -144,25 +119,4 @@ function parseDateTime(dateTimeString) {
     date.setUTCDate(day);
 
     return date;
-}
-
-async function makeScrollingText(title, titleDelay, rate, boundaryWidth, padding) {
-    if (title.scrollWidth > boundaryWidth) {
-        titleDelay.innerHTML = title.innerHTML;
-		let ratio = (title.scrollWidth/boundaryWidth)*rate
-		title.style.animation = `scrollText ${ratio}s linear infinite`;
-		titleDelay.style.animation = `scrollText ${ratio}s linear infinite`;
-		titleDelay.style.animationDelay = `${-ratio/2}s`;
-		titleDelay.style.paddingRight = `${padding}px`;
-		title.style.paddingRight = `${padding}px`;
-        titleDelay.style.marginTop = `-${title.offsetHeight}px`;
-        titleDelay.style.display = "initial";
-    } else {
-        titleDelay.style.display = "none";
-		title.style.animation = "none";
-		titleDelay.style.animation = "none";
-		titleDelay.style.paddingRight = "0px";
-        titleDelay.style.marginTop = `0px`;
-		title.style.paddingRight = "0px";
-	}
 }
